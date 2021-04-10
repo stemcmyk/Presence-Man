91a676a7c410ec96d25f91b3e678adb9bd41b8f1
Noel Zayas <51480428+stemcmyk@users.noreply.github.com>
2021-04-10 20:56:55 +0200

    Added "Date.getWeek", will return current week of the year

--- basic_functions.js
+++ basic_functions.js
@@ -53,5 +53,19 @@ var sortTable = (table, max) => {
   }
   return srf;
 };
-
+Date.prototype.getWeek = function () {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 3 - ((date.getDay() + 6) % 7));
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  return (
+    1 +
+    Math.round(
+      ((date.getTime() - week1.getTime()) / 86400000 -
+        3 +
+        ((week1.getDay() + 6) % 7)) /
+        7
+    )
+  );
+};
 module.exports = { format_time, sortTable };
